4fb913c8075c0da8e9a34ff83c6b66540cae50e6
Kieron Peters <kieronpeters1@gmail.com>
2020-02-20 20:07:37 +0000

    test2 - added new read of data and unit tests to file

--- TechnicalTestABDDataSystems/Test2XML.cs
+++ TechnicalTestABDDataSystems/Test2XML.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Xml;
 using System.Xml.Serialization;
 
@@ -10,22 +10,83 @@ namespace TechnicalTestABDDataSystems
     class Test2XML
     {
 
-        private static string Xml = @"<InputDocument><DeclarationList><Declaration Command=""DEFAULT"" Version=\""5.13""><DeclarationHeader><Jurisdiction>IE</Jurisdiction><CWProcedure>IMPORT</CWProcedure><DeclarationDestination>CUSTOMSWAREIE</DeclarationDestination><DocumentRef>71Q0019681</DocumentRef><SiteID>DUB</SiteID><AccountCode>G0779837</AccountCode><Reference RefCode = ""MWB"" >< RefText > 586133622 </ RefText ></ Reference >< Reference RefCode=""KEY""><RefText>DUB16049</RefText></Reference><Reference RefCode = ""CAR"" >< RefText > 71Q0019681</RefText></Reference><Reference RefCode = ""COM"" >< RefText > 71Q0019681</RefText></Reference><Reference RefCode = ""SRC"" >< RefText > ECUS </ RefText ></ Reference >< Reference RefCode=""TRV""><RefText>1</RefText></Reference><Reference RefCode = ""CAS"" >< RefText > 586133622 </ RefText ></ Reference >< Reference RefCode=""HWB""><RefText>586133622</RefText></Reference><Reference RefCode = ""UCR"" >< RefText > 586133622 </ RefText ></ Reference >< Country CodeType=""NUM"" CountryType=""Destination"" > IE</Country><Country CodeType = ""NUM"" CountryType=""Dispatch"">CN</Country></DeclarationHeader></DeclarationList></InputDocument>";
-
         public bool test2_Xml()
         {
-            LoadFromXMLString(Xml);
-            Console.WriteLine();
+            // storage variables and using reference so we can later unit test methods with bool returns
+            InputDocument xmlFileObject = new InputDocument();
+            List<string[]> targetXMLContents = new List<string[]>();
+            // small unit tests on code running
+            bool loadedFile = LoadFromXMLFile(ref xmlFileObject);
+            bool harvestedData = HarvestTargetXMLElements(ref xmlFileObject, ref targetXMLContents);
+            bool printedData = PrintTargetXMLContent(ref targetXMLContents);
+            Test2_UnitTests(loadedFile, harvestedData, printedData);
+           
+
             return true;
         }
 
-        private bool LoadFromXMLString(string xml)
+        private void Test2_UnitTests(bool loadedFile, bool harvestedData, bool printedData)
         {
+            Assert.IsTrue(loadedFile);
+            Assert.IsTrue(harvestedData);
+            Assert.IsTrue(printedData);
+        }
+
+        private bool LoadFromXMLFile(ref InputDocument xmlFileObject)
+        {
+            // we use a serializer to read XML formatting into our generated class file we made from the XML (InputDocument)
             XmlSerializer serializer = new XmlSerializer(typeof(InputDocument));
-            InputDocument resultingMessage = (InputDocument)serializer.Deserialize(new XmlTextReader(Path.Combine(Environment.CurrentDirectory, @"content.xml")));
-            Console.WriteLine(resultingMessage.DeclarationList.Declaration.Command);
+            // Using a path to get current file in the context of the project directory path
+            xmlFileObject = (InputDocument)serializer.Deserialize(new XmlTextReader(Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, @"..\..\..\content.xml"))));
             return true;
 
+        }
+
+        private bool HarvestTargetXMLElements(ref InputDocument xmlFileObject, ref List<string[]> targetXMLContents)
+        {
+            try
+            {
+                // collecting all xml reference tags from the object
+                InputDocumentDeclarationListDeclarationDeclarationHeaderReference[] references = xmlFileObject.DeclarationList.Declaration.DeclarationHeader.Reference;
+
+                foreach (InputDocumentDeclarationListDeclarationDeclarationHeaderReference reference in references)
+                {
+                    // finding target tags with the three refcodes and adding to our list to save targetXMLcontents
+                    if (reference.RefCode.Equals("MWB") || reference.RefCode.Equals("TRV") || reference.RefCode.Equals("CAR"))
+                    {
+                        targetXMLContents.Add(new string[] { reference.RefCode, reference.RefText });
+                    }
+                }
+
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool PrintTargetXMLContent(ref List<string[]> targetXMLContents)
+        {
+            int i = 1;
+
+            foreach (string[] targetXMLContent in targetXMLContents)
+            {
+                // print out lines of the XML contents that were found
+                try
+                {
+                    Console.WriteLine("Result " + i + ": RefCode of " + targetXMLContent[0] + " contains " + targetXMLContent[1]);
+                    i++;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 
